275479788a8d899ef6a32ff98f218da23bf186e7
Veetaha <gersoh3@gmail.com>
2024-06-30 15:55:44 +0000

    Move prox into a separate repo

--- prox/src/printing.rs
+++
@@ -1,9 +0,0 @@
-/// Uses [`prettyplease`] to format [`syn::Path`] to [`String`].
-pub fn path_to_string(path: &syn::Path) -> String {
-    prettyplease::unparse(&syn::parse_quote!(use #path;))
-        .strip_prefix("use ")
-        .unwrap()
-        .strip_suffix(";\n")
-        .unwrap()
-        .to_owned()
-}
--- prox/src/test/mod.rs
+++
@@ -1,3 +0,0 @@
-mod snapshot;
-
-pub use snapshot::*;
