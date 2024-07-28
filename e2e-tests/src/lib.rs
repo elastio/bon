74aa361aaab6a1853717f8454d03f3a9c8a6dad3
Veetaha <gersoh3@gmail.com>
2024-07-28 18:19:51 +0000

    Fix doc tests

--- e2e-tests/src/lib.rs
+++ e2e-tests/src/lib.rs
@@ -6,6 +6,10 @@
 use bon::{bon, builder};
 
 #[cfg(doctest)]
+// We use a bunch of Vitepress-specific syntax in the doctests, for example to
+// give a name to a code group in a fenced code block, which conflicts with this
+// lint.
+#[allow(rustdoc::invalid_codeblock_attributes)]
 mod website_doctests {
     include!(concat!(env!("OUT_DIR"), "/website_doctests.rs"));
 }
