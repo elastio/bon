e919af7b28dc8af2a9ae588b829b0c735d77ef3f
Veetaha <gersoh3@gmail.com>
2024-09-07 18:28:04 +0000

    Better tests

--- bon/tests/integration/builder/cfgs.rs
+++ bon/tests/integration/builder/cfgs.rs
@@ -85,19 +85,26 @@ fn fn_with_params() {
         #[cfg_attr(all(), builder(default))] arg2: [u8; 4],
 
         #[cfg_attr(any(), builder(name = renamed))] arg3: [char; 2],
-
-        #[cfg_attr(all(), cfg_attr(not(not(all())), builder(name = renamed)))] arg5: u32,
-    ) -> (bool, u32) {
-        let _ = (arg2, arg3);
-        (arg1, arg5)
+    ) -> (bool, [u8; 4], [char; 2]) {
+        (arg1, arg2, arg3)
     }
 
     let builder: OverrideBuilder = sut();
 
     assert_debug_eq(
-        builder.arg1(true).arg3(['a', 'b']).renamed(32).call(),
-        expect!["(true, 32)"],
+        builder.arg1(true).arg3(['a', 'b']).call(),
+        expect!["(true, [0, 0, 0, 0], ['a', 'b'])"],
     );
+}
+
+#[test]
+fn fn_with_nested_cfgs() {
+    #[builder]
+    fn sut(#[cfg_attr(all(), cfg_attr(not(not(all())), builder(name = renamed)))] arg: u32) -> u32 {
+        arg
+    }
+
+    assert_eq!(sut().renamed(99).call(), 99);
 }
 
 #[test]
