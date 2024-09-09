cf1f90f54fd0f24f4ebc7689a1b6dcae7451217b
Veetaha <gersoh3@gmail.com>
2024-09-09 22:37:32 +0000

    Fix website build

--- website/data/version.data.ts
+++ website/data/version.data.ts
@@ -2,10 +2,10 @@ import toml from "smol-toml";
 import fs from "node:fs/promises";
 
 export default {
-    watch: ['../../Cargo.toml'],
+    watch: ['../../bon/Cargo.toml'],
     async load([cargoTomlPath]: [string]) {
         const cargoTomlContent = await fs.readFile(cargoTomlPath, "utf-8");
         const cargoToml = toml.parse(cargoTomlContent) as any;
-        return cargoToml.workspace.package.version;
+        return cargoToml.package.version;
     }
 }
