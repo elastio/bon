5f63ff61829415aec40f00be750a56e800910e56
Veetaha <gersoh3@gmail.com>
2024-11-27 00:55:05 +0000

    Add a comment

--- bon/tests/integration/ui/compile_fail/private_fields_access.rs
+++ bon/tests/integration/ui/compile_fail/private_fields_access.rs
@@ -11,6 +11,10 @@ impl Sut {
 fn main() {
     let sut = Sut.sut(99);
 
+    // Previously, there was an attempt to generate names for private fields
+    // with randomness to ensure users don't try to access them. This however,
+    // conflicts with caching in some build systems. See the following issue
+    // for details: https://github.com/elastio/bon/issues/218
     let SutSutBuilder {
         __unsafe_private_phantom: _,
         __unsafe_private_start_fn_args: _,
