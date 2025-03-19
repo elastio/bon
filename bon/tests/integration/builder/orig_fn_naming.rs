6d66362727ee6908ea4db5484510a3ffbe1aa07b
Veetaha <gersoh3@gmail.com>
2025-03-19 20:55:26 +0000

    Maket  tests `#[no_std]`-compatible

--- bon/tests/integration/builder/orig_fn_naming.rs
+++ bon/tests/integration/builder/orig_fn_naming.rs
@@ -10,18 +10,18 @@ fn attributes_see_orig_fn_name() {
         ) => {
             $(#[$attr])*
             fn $fn_name() -> $ret {
-                stringify!($fn_name).to_string()
+                stringify!($fn_name)
             }
         };
     }
 
     #[builder]
     #[apply(return_fn_name!)]
-    fn attr_after_builder() -> String {}
+    fn attr_after_builder() -> &'static str {}
 
     #[apply(return_fn_name!)]
     #[builder]
-    fn attr_before_builder() -> String {}
+    fn attr_before_builder() -> &'static str {}
 
     struct Sut;
 
@@ -29,30 +29,30 @@ fn attributes_see_orig_fn_name() {
     impl Sut {
         #[builder]
         #[apply(return_fn_name!)]
-        fn attr_after_builder() -> String {}
+        fn attr_after_builder() -> &'static str {}
 
         #[apply(return_fn_name!)]
         #[builder]
-        fn attr_before_builder() -> String {}
+        fn attr_before_builder() -> &'static str {}
     }
 
     let actual = attr_after_builder().call();
     let expected = expect!["attr_after_builder"];
 
-    expected.assert_eq(&actual);
+    expected.assert_eq(actual);
 
     let actual = attr_before_builder().call();
     let expected = expect!["attr_before_builder"];
 
-    expected.assert_eq(&actual);
+    expected.assert_eq(actual);
 
     let actual = Sut::attr_after_builder().call();
     let expected = expect!["attr_after_builder"];
 
-    expected.assert_eq(&actual);
+    expected.assert_eq(actual);
 
     let actual = Sut::attr_before_builder().call();
     let expected = expect!["attr_before_builder"];
 
-    expected.assert_eq(&actual);
+    expected.assert_eq(actual);
 }
