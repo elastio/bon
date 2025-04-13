52b76c4e261dd9e06cdfeba8de12e7ea10f153ab
Veetaha <gersoh3@gmail.com>
2025-04-13 14:04:42 +0000

    Fixes

--- scripts/test-msrv.sh
+++ scripts/test-msrv.sh
@@ -34,6 +34,9 @@ with_log cd bon
 
 step echo '[workspace]' >> Cargo.toml
 
+step cargo update -p once_cell --precise 1.17.2
+step cargo update -p serde --precise 1.0.194
+step cargo update -p trybuild --precise 1.0.89
 step cargo update -p prettyplease --precise 0.2.17
 step cargo update -p syn --precise 2.0.56
 step cargo update -p tokio --precise 1.29.1
