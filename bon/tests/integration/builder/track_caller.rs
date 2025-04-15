cc3db71ea1c6396e7a5874997936f17eed289040
Lilyyy411 <lilyyy4111@gmail.com>
2025-04-15 15:51:54 -0400

    Fix track_caller integration test to not include file path.

--- bon/tests/integration/builder/track_caller.rs
+++ bon/tests/integration/builder/track_caller.rs
@@ -1,41 +1,41 @@
 use crate::prelude::*;
 use core::panic::Location;
 
+#[derive(Debug)]
+#[allow(dead_code)]
+struct LineCol {
+    line: u32,
+    col: u32,
+}
+
+impl From<&'_ Location<'_>> for LineCol {
+    fn from(value: &'_ Location<'_>) -> Self {
+        Self {
+            line: value.line(),
+            col: value.column(),
+        }
+    }
+}
+
 #[test]
 fn track_caller_fn() {
     #[builder]
     #[track_caller]
-    fn dont_brick(_x: u32) -> &'static Location<'static> {
-        Location::caller()
+    fn dont_brick(_x: u32) -> LineCol {
+        Location::caller().into()
     }
 
     let location = dont_brick().x(10).call();
-    assert_debug_eq(
-        location,
-        expect![[r#"
-        Location {
-            file: "bon/tests/integration/builder/track_caller.rs",
-            line: 12,
-            col: 39,
-        }"#]],
-    );
+    assert_debug_eq(location, expect!["LineCol { line: 28, col: 39 }"]);
 
     #[track_caller]
     #[builder]
-    fn dont_brick_2(_x: u32) -> &'static Location<'static> {
-        Location::caller()
+    fn dont_brick_2(_x: u32) -> LineCol {
+        Location::caller().into()
     }
 
     let location = dont_brick_2().x(10).call();
-    assert_debug_eq(
-        location,
-        expect![[r#"
-            Location {
-                file: "bon/tests/integration/builder/track_caller.rs",
-                line: 29,
-                col: 41,
-            }"#]],
-    );
+    assert_debug_eq(location, expect!["LineCol { line: 37, col: 41 }"]);
 }
 
 #[test]
@@ -46,19 +46,11 @@ fn track_caller_impl_block() {
     impl Senti {
         #[builder(finish_fn = yatta)]
         #[track_caller]
-        fn new(brick: Brick) -> &'static Location<'static> {
+        fn new(brick: Brick) -> LineCol {
             let Brick = brick;
-            Location::caller()
+            Location::caller().into()
         }
     }
     let yatta = Senti::builder().brick(Brick).yatta();
-    assert_debug_eq(
-        yatta,
-        expect![[r#"
-            Location {
-                file: "bon/tests/integration/builder/track_caller.rs",
-                line: 54,
-                col: 47,
-            }"#]],
-    );
+    assert_debug_eq(yatta, expect!["LineCol { line: 54, col: 47 }"]);
 }
