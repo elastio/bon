7c9f53a96b226828a8a0d2191fef868e3cd005cf
Veetaha <gersoh3@gmail.com>
2025-08-09 17:05:00 +0000

    Add support for non-static lifetimes

--- bon-macros/src/builder/builder_gen/builder_derives/into_future.rs
+++ bon-macros/src/builder/builder_gen/builder_derives/into_future.rs
@@ -1,6 +1,9 @@
 use crate::builder::builder_gen::models::BuilderGenCtx;
 use crate::builder::builder_gen::top_level_config::IntoFutureConfig;
 use crate::util::prelude::*;
+use std::borrow::Cow;
+use std::collections::BTreeSet;
+use syn::visit_mut::VisitMut;
 
 impl BuilderGenCtx {
     pub(super) fn derive_into_future(&self, config: &IntoFutureConfig) -> Result<TokenStream> {
@@ -33,25 +36,25 @@ impl BuilderGenCtx {
             );
         }
 
-        let output_ty = match &self.finish_fn.output {
-            syn::ReturnType::Default => Box::new(syn::Type::Tuple(syn::TypeTuple {
-                paren_token: syn::token::Paren::default(),
-                elems: syn::punctuated::Punctuated::new(),
-            })),
-            syn::ReturnType::Type(_, output_ty) => output_ty.clone(),
-        };
-
         let state_mod = &self.state_mod.ident;
-        let generics_decl = &self.generics.decl_without_defaults;
-        let generic_args = &self.generics.args;
         let builder_ident = &self.builder_type.ident;
         let state_var = &self.state_var;
         let finish_fn_ident = &self.finish_fn.ident;
         let box_ = &config.box_ident;
 
-        let builder_ty = quote! {
-            #builder_ident<#(#generic_args,)* #state_var>
-        };
+        let SignatureForIntoFuture {
+            generics_decl,
+            generic_args,
+            where_clause,
+            builder_lifetime,
+            output_ty,
+        } = self.signature_for_into_future();
+
+        let state_lifetime = builder_lifetime
+            .clone()
+            .unwrap_or_else(|| syn::Lifetime::new("'static", Span::call_site()));
+
+        let builder_lifetime = Option::into_iter(builder_lifetime);
 
         let send_bound = if config.is_send {
             quote! { + ::core::marker::Send }
@@ -77,13 +80,17 @@ impl BuilderGenCtx {
             #[automatically_derived]
             impl<
                 #(#generics_decl,)*
-                #state_var: #state_mod::IsComplete + 'static
+                #state_var: #state_mod::IsComplete + #state_lifetime
             >
-            ::core::future::IntoFuture for #builder_ty {
+            ::core::future::IntoFuture for #builder_ident<#(#generic_args,)* #state_var>
+            #where_clause
+            {
                 type Output = #output_ty;
                 type IntoFuture = ::core::pin::Pin<
                     #alloc::boxed::#box_<
-                        dyn ::core::future::Future<Output = #output_ty> #send_bound
+                        dyn ::core::future::Future<Output = Self::Output>
+                        #send_bound
+                        #(+ #builder_lifetime)*
                     >
                 >;
 
@@ -94,5 +101,115 @@ impl BuilderGenCtx {
         };
 
         Ok(tokens)
+    }
+
+    /// Handle the special case for a builder that captures lifetimes.
+    ///
+    /// Collapse all lifetimes into a single `'builder` lifetime. This is
+    /// because `dyn Trait` supports only a single `+ 'lifetime` bound.
+    fn signature_for_into_future(&self) -> SignatureForIntoFuture<'_> {
+        let generics_decl = &self.generics.decl_without_defaults;
+        let generic_args = &self.generics.args;
+        let where_clause = &self.generics.where_clause;
+
+        let output_ty = match &self.finish_fn.output {
+            syn::ReturnType::Default => Cow::Owned(syn::parse_quote!(())),
+            syn::ReturnType::Type(_, output_ty) => Cow::Borrowed(output_ty.as_ref()),
+        };
+
+        let contains_lifetimes = matches!(
+            self.generics.args.first(),
+            Some(syn::GenericArgument::Lifetime(_))
+        );
+
+        if !contains_lifetimes {
+            return SignatureForIntoFuture {
+                generics_decl: Cow::Borrowed(generics_decl),
+                generic_args: Cow::Borrowed(generic_args),
+                where_clause: where_clause.as_ref().map(Cow::Borrowed),
+                builder_lifetime: None,
+                output_ty,
+            };
+        }
+
+        let builder_lifetime = syn::Lifetime::new("'builder", Span::call_site());
+
+        let new_generic_args = generic_args
+            .iter()
+            .map(|arg| match arg {
+                syn::GenericArgument::Lifetime(_) => {
+                    syn::GenericArgument::Lifetime(builder_lifetime.clone())
+                }
+                _ => arg.clone(),
+            })
+            .collect::<Vec<_>>();
+
+        let mut original_lifetimes = BTreeSet::new();
+        let mut new_generics_decl = vec![syn::parse_quote!(#builder_lifetime)];
+
+        for param in generics_decl {
+            match param {
+                syn::GenericParam::Lifetime(lifetime) => {
+                    original_lifetimes.insert(&lifetime.lifetime.ident);
+                }
+                _ => {
+                    new_generics_decl.push(param.clone());
+                }
+            }
+        }
+
+        let mut replace_lifetimes = ReplaceLifetimes {
+            replacement: &builder_lifetime,
+            original_lifetimes: &original_lifetimes,
+        };
+
+        let mut new_where_clause = where_clause.clone();
+
+        if let Some(where_clause) = &mut new_where_clause {
+            replace_lifetimes.visit_where_clause_mut(where_clause);
+        }
+
+        let mut output_ty = output_ty.into_owned();
+
+        replace_lifetimes.visit_type_mut(&mut output_ty);
+
+        SignatureForIntoFuture {
+            generics_decl: Cow::Owned(new_generics_decl),
+            generic_args: Cow::Owned(new_generic_args),
+            where_clause: new_where_clause.map(Cow::Owned),
+            builder_lifetime: Some(builder_lifetime),
+            output_ty: Cow::Owned(output_ty),
+        }
+    }
+}
+
+struct SignatureForIntoFuture<'a> {
+    generics_decl: Cow<'a, [syn::GenericParam]>,
+    generic_args: Cow<'a, [syn::GenericArgument]>,
+    where_clause: Option<Cow<'a, syn::WhereClause>>,
+    builder_lifetime: Option<syn::Lifetime>,
+    output_ty: Cow<'a, syn::Type>,
+}
+
+struct ReplaceLifetimes<'a> {
+    replacement: &'a syn::Lifetime,
+    original_lifetimes: &'a BTreeSet<&'a syn::Ident>,
+}
+
+impl VisitMut for ReplaceLifetimes<'_> {
+    fn visit_lifetime_mut(&mut self, lifetime: &mut syn::Lifetime) {
+        if self.original_lifetimes.contains(&lifetime.ident) {
+            *lifetime = self.replacement.clone();
+        }
+    }
+
+    fn visit_item_mut(&mut self, _: &mut syn::Item) {
+        // Don't recurse into child items. They don't inherit the parent item's
+        // lifetimes.
+    }
+
+    fn visit_bound_lifetimes_mut(&mut self, _: &mut syn::BoundLifetimes) {
+        // Don't recurse into bound lifetime declarations. They introduce
+        // local lifetimes that we should keep as is
     }
 }
--- bon/tests/integration/builder/attr_into_future.rs
+++ bon/tests/integration/builder/attr_into_future.rs
@@ -84,3 +84,49 @@ async fn into_future_with_optional() {
     let result = optional_param().await;
     assert_eq!(result, 100);
 }
+
+#[tokio::test]
+async fn references_in_params() {
+    struct Dummy;
+
+    #[builder(derive(IntoFuture(Box)))]
+    async fn sut<'named1, 'named2>(
+        _x1: &Dummy,
+        _x2: &Dummy,
+        x3: &'named1 Dummy,
+        x4: &'named2 Dummy,
+    ) -> &'named2 Dummy {
+        let _: &'named1 Dummy = x3;
+        ready(x4).await
+    }
+
+    // Store the dummy struct in local variables to make sure no `'static`
+    // lifetime promotion happens
+    let local_x1 = Dummy;
+    let local_x2 = Dummy;
+    let local_x3 = Dummy;
+    let local_x4 = Dummy;
+
+    let _: &Dummy = sut()
+        .x1(&local_x1)
+        .x2(&local_x2)
+        .x3(&local_x3)
+        .x4(&local_x4)
+        .await;
+}
+
+#[tokio::test]
+async fn anon_lifetime_in_return_type() {
+    struct Dummy;
+
+    #[builder(derive(IntoFuture(Box)))]
+    async fn sut(x1: &Dummy) -> &Dummy {
+        ready(x1).await
+    }
+
+    // Store the dummy struct in local variables to make sure no `'static`
+    // lifetime promotion happens
+    let local_x1 = Dummy;
+
+    let _: &Dummy = sut().x1(&local_x1).await;
+}
