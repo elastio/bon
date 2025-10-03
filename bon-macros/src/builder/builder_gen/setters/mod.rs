c9712b389ae74c40b07b779146fa8b06beed62fa
Veetaha <gersoh3@gmail.com>
2025-10-03 12:00:51 +0000

    Even better

--- bon-macros/src/builder/builder_gen/setters/mod.rs
+++ bon-macros/src/builder/builder_gen/setters/mod.rs
@@ -455,16 +455,13 @@ impl<'a> SettersCtx<'a> {
         let types = imp.inputs.iter().map(|(_, ty)| ty);
         let const_ = &self.base.const_;
 
-        let fn_modifiers = quote!(#vis #const_)
-            // Respan the tokens to make sure the function's signature begins with
-            // the member's span. This is important to make rustdoc's source links
-            // point to the original member's location.
-            .into_iter()
-            .map(|mut token| {
-                token.set_span(span);
-                token
-            })
-            .collect::<TokenStream>();
+        // Respan the tokens to make sure the function's signature begins with
+        // the member's span. This is important to make rustdoc's source links
+        // point to the original member's location.
+        let fn_modifiers = quote!(#vis #const_).into_iter().map(|mut token| {
+            token.set_span(span);
+            token
+        });
 
         quote_spanned! {span=>
             #( #docs )*
@@ -479,7 +476,7 @@ impl<'a> SettersCtx<'a> {
                 clippy::missing_const_for_fn,
             )]
             #[inline(always)]
-            #fn_modifiers fn #name(#maybe_mut self, #( #pats: #types ),*) -> #return_type
+            #(#fn_modifiers)* fn #name(#maybe_mut self, #( #pats: #types ),*) -> #return_type
             #where_clause
             {
                 #body
