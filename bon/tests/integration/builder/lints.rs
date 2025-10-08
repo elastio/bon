ba663a43a16e62c58cfaf7b2af9746e579466fe7
Veetaha <gersoh3@gmail.com>
2025-10-08 11:50:39 +0000

    Add a regression test for `clippy::wrong_self_convention`

--- bon/tests/integration/builder/lints.rs
+++ bon/tests/integration/builder/lints.rs
@@ -10,3 +10,16 @@ fn func_with_skipped_generic_arg() {
 
     sut().arg(&32).call();
 }
+
+// Test for https://github.com/elastio/bon/pull/349
+#[test]
+fn clippy_wrong_self_convention() {
+    // This used to trigger the `clippy::wrong_self_convention` lint
+    #[derive(Builder)]
+    struct Sut {
+        is_unique: bool,
+    }
+
+    let value = Sut::builder().is_unique(true).build();
+    let _ = value.is_unique;
+}
